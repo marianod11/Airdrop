8208f876206cc575662f500e3977f281b75362c8
Mariano Dell Aquila <48595875+marianod11@users.noreply.github.com>
2022-05-16 20:44:09 -0300

    Update Airdrop.sol

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -17,13 +17,13 @@ contract Airdrop {
 
 
     function addAddressToWhitelist() public {
-        // check if the user has already been whitelisted
+       
         require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
-        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+    
         require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
-        // Add the address which called the function to the whitelistedAddress array
+
         whitelistedAddresses[msg.sender] = true;
-        // Increase the number of whitelisted addresses
+    
         numAddressesWhitelisted += 1;
 
         totalAddressWhiteList.push(msg.sender);
@@ -32,11 +32,11 @@ contract Airdrop {
 
 
     function removeAddressToWhitelist() public {
-        // check if the user has already been whitelisted
+  
         require(whitelistedAddresses[msg.sender], "no estaaaaaa"); 
-        // Add the address which called the function to the whitelistedAddress array
+      
         whitelistedAddresses[msg.sender] = false;
-        // Increase the number of whitelisted addresses
+  
         numAddressesWhitelisted -= 1;
         
         for(uint256 i = 0; i < totalAddressWhiteList.length; i++ ){
@@ -56,9 +56,9 @@ contract Airdrop {
 
     function airdrop() public payable {
         uint balance = address(this).balance;
-        //total de cuentas
+    
         uint totalAcounts = totalAddressWhiteList.length;
-        //total a pagar
+     
         uint totalPagar = balance / totalAcounts ;
 
         for(uint i = 0; i <totalAddressWhiteList.length; i++ ){
